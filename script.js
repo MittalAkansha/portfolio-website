f1c1929fe352bee949c6f5d65f7747bd758b873f
Ajjansha <141652880+Ajjansha@users.noreply.github.com>
2026-09-14 01:12:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const name = document.querySelector('input[type="text"]').value;
+    const email = document.querySelector('input[type="email"]').value;
+    const message = document.querySelector("textarea").value;
+
+    if (name === "" || email === "" || message === "") {
+        alert("Please fill all fields.");
+        return;
+    }
+
+    alert("Thank you! Your message has been sent.");
+});
